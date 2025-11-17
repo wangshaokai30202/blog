28c986e59170c353fda90b5f494778be5a14cf33
wangshaokai30202 <166330547@qq.com>
2025-11-17 23:04:12 +0800

    Site updated: 2025-11-17 23:04:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/10/如何利用hexo-GitHub-Zeabur搭建免费博客/","2025/11/10/我的第一篇博文/","2025/11/10/hello-world/"];function toRandomPost(){
+var posts=["2025/11/10/我的第一篇博文/","2025/11/10/hello-world/","2025/11/10/如何利用hexo-GitHub-Zeabur搭建免费博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
